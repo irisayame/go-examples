1b3cd6ca5a10a01eda52da5f7c68aab495ea51f4
irisayame <xinni.ge1990@gmail.com>
2015-02-02 17:15:15 +0800

    chan and others

--- tickers.go
+++ tickers.go
@@ -4,4 +4,14 @@ import "time"
 import "fmt"
 
 func main() {
+	ticker := time.NewTicker(time.Millisecond * 1000)
+	go func() {
+		for t := range ticker.C {
+			fmt.Println("Tick at", t)
+		}
+	}()
+
+	time.Sleep(time.Millisecond * 5000)
+	ticker.Stop()
+	fmt.Println("Ticker stopped")
 }
